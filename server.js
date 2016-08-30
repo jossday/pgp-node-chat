c09b0730d5a445f0b49786cc7c9d55b027fee383
jossday <undergroundempire7@gmail.com>
2016-08-30 21:52:31 +0200

    Setup of MongoDB

--- server.js
+++ server.js
@@ -1,50 +1,26 @@
+/*----HTTP Server----*/
 var http = require("http");
-var mongoClient = require("mongodb").MongoClient;
-var openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
-openpgp.initWorker({ path:'openpgp.worker.js' }) // set the relative web worker path
-openpgp.config.aead_protect = true // activate fast AES-GCM mode (experimental)
-var options, encrypted;
-
-// Paramètres de connexion
-var url = 'mongodb://localhost/messaging';
-
-// Connexion au serveur avec la méthode connect
-mongoClient.connect(url, function (err, db) {
-    if (err) {
-        return console.error('Connection failed', err);
-    }
-    console.log('Connection successful on ', url);
-
-    // Récupération de la collection users
-    var collection = db.collection('users');
-
-    // Création de deux objets users
-    var user1 = {login: 'blasphemy', pass: 'test', email: 'undergroundempire7@gmail.com', token: ''};
-    var user2 = {login: 'troisieme', pass: 'test', email: 'trucalacon@gmail.com', token: ''};
-
-    // Enregistrement de plusieurs objets en db avec insertMany
-    collection.insertMany([user1, user2], function (err, result) {
-        if (err) {
-            console.error('Insert failed', err);
-        } else {
-            console.log('Insert successful', result);
-        }
-
-        db.close()
-    });
-
-    // Fermeture de la connexion
-    db.close()
-});
-
-
-
-/*
 httpServer = http.createServer(function(req, res){
   console.log("Server is running");
 });
 httpServer.listen(8080);
 
+/*-----MongoDB-------*/
+var mongoClient = require("mongodb").MongoClient;
+var url = 'mongodb://localhost/messaging';
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+var user1;
+var user2;
+var password = 'test';
+
+/*-----OpenPGP-------*/
+var openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
+openpgp.initWorker({ path:'openpgp.worker.js' }) // set the relative web worker path
+openpgp.config.aead_protect = true // activate fast AES-GCM mode (experimental)
+var options, encrypted;
+
+/*----Sockets-------*/
 var io = require("socket.io").listen(httpServer);
 var users = {};
 
@@ -58,6 +34,54 @@ io.sockets.on("connection", function(socket){
   }
 
   socket.on('login', function(user){
+    mongoClient.connect(url, function (err, db) {
+    if (err) {
+        return console.error('Connection failed', err);
+    }
+    console.log('Connection successful on ', url);
+
+    var findDocuments = function(db, callback) {
+      // Get the documents collection
+      var collection = db.collection('users');
+      // Find some documents
+      collection.find({'login': [users.login]}).toArray(function(err, docs) {
+        if(err){
+          socket.emit('error', err);
+        }
+        /*if(rows.length === 1 && rows[0].token === user.token){
+          me = {};
+          me.id = rows[0].id;
+          me.username = rows[0].login;
+          socket.emit('logged');
+          users[me.id] = me;
+          password = rows[0].pass;
+          io.sockets.emit('newusr', me);
+        }*/
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+      });      
+    }
+
+    /*
+     bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+        user1 = {login: 'blasphemy', pass: hash, email: 'undergroundempire7@gmail.com', token: ''};
+        user2 = {login: 'troisieme', pass: hash, email: 'trucalacon@gmail.com', token: ''};
+
+        collection.insertMany([user1, user2], function (err, result) {
+            if (err) {
+                console.error('Insert failed', err);
+            } else {
+                console.log('Insert successful', result);
+            }
+
+            db.close()
+        });
+      });
+     });*/
+    });
     connection.query('SELECT * FROM users WHERE id = ?', [user.id], function(err, rows, fields){
       if(err){
         socket.emit('error', err);
@@ -76,7 +100,8 @@ io.sockets.on("connection", function(socket){
       }
     });
   });
-
+});
+/*
   socket.on('loadMsg', function(emetteur, destinataire){
     messages = [];
     connection.query('SELECT users.id AS user_id, users.login, messages.texte, messages.id_user2 AS id_user2, UNIX_TIMESTAMP(messages.date) AS date FROM messages LEFT JOIN users ON users.id = messages.id_user WHERE (messages.id_user = ? AND messages.id_user2 = ?) OR (messages.id_user = ? AND messages.id_user2 = ?) ORDER BY date LIMIT 10',
@@ -147,6 +172,4 @@ io.sockets.on("connection", function(socket){
     }
     delete users[me.id];
     io.sockets.emit('disusr', me);
-  });
-});
-*/
+  });*/
